0e9a325927cdd1cad8cbc2d66261e3fb5f71ce15
Jorge <wiijlg@hotmail.com>
2017-09-06 01:12:09 +0200

    Ocultar sección Suscritos
    Sección no terminada, permanece oculta hasta ser terminada correctamente

--- plantilla/menu-lateral.php
+++ plantilla/menu-lateral.php
@@ -47,7 +47,6 @@ include 'conexion.php';
   <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
     <a class="mdl-navigation__link" href="index.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Inicio</a>
     <a class="mdl-navigation__link" href="perfil.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">account_box</i>Perfil</a>
-    <a class="mdl-navigation__link" href="suscriptores.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">people_outline</i>Suscriptores</a>
     <a class="mdl-navigation__link" href="suscribirse.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">people</i>Suscribirse</a>
     <a class="mdl-navigation__link" href="videos-subs.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">movie</i>Videos de usuarios</a>
     <a class="mdl-navigation__link" href="reportes.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">report</i>Reportes</a>
