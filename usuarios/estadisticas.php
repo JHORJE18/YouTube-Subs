3f087bdabfaafaff8fa1fb36b0dbd74399b6808d
Jorge <wiijlg@hotmail.com>
2017-09-06 13:55:56 +0200

    Corregido estadisticas perfil

--- usuarios/estadisticas.php
+++ usuarios/estadisticas.php
@@ -28,7 +28,7 @@
             <span>Suscripciones conseguidas:</span>
         </span>
         <span class="mdl-list__item-secondary-content">
-            <strong><?php echo $perfilUser[]9® ?></strong>
+            <strong><?php echo $perfilUser[9] ?></strong>
         </span>
     </li>
     <li class="mdl-list__item">
